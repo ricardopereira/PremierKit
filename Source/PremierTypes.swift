982d21cd7a9cf11c53ab9ad12f12cbfec0d1df08
Ricardo Pereira <m@ricardopereira.eu>
2016-11-24 18:07:56 +0000

    Result enum with success and failure convenience properties

--- Source/PremierTypes.swift
+++ Source/PremierTypes.swift
@@ -31,4 +31,22 @@ public enum Result<T, E: ErrorType> {
     public init(error: E) {
         self = .Failure(error)
     }
+
+    public var success: T? {
+        switch self {
+        case .Success(let value):
+            return value
+        case .Failure(_):
+            return nil
+        }
+    }
+
+    public var failure: E? {
+        if case .Failure(let error) = self {
+            return error
+        }
+        else {
+            return nil
+        }
+    }
 }
