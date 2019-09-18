5b01d90a5970fe93860f7db556612a2df1b85329
Ricardo Pereira <m@ricardopereira.eu>
2019-09-18 23:44:41 +0100

    UIKit dependencies

--- Source/Premier+UIDeviceOrientation.swift
+++ Source/Premier+UIDeviceOrientation.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Ricardo Pereira. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 extension UIDeviceOrientation: CustomStringConvertible {
 
--- Source/Premier+UIFont.swift
+++ Source/Premier+UIFont.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Ricardo Pereira. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 extension UIFont {
 
