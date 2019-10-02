9fe47d948dba33f2f04d9b62d5a7907dc8d74ff6
Ricardo Pereira <m@ricardopereira.eu>
2019-10-02 01:01:28 +0100

    UserDefaults property wrapper

---
+++ Source/Premier+UserDefaults.swift
@@ -0,0 +1,29 @@
+//
+//  Premier+UserDefaults.swift
+//  PremierKit
+//
+//  Created by Ricardo Pereira on 01/10/2019.
+//  Copyright © 2019 Ricardo Pereira. All rights reserved.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefault<T> {
+    let key: String
+    let defaultValue: T
+
+    init(_ key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
