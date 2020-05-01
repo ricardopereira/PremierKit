ebf5d95fc59ae19f74fcd9409e94eb88e395421c
Ricardo Pereira <m@ricardopereira.eu>
2020-05-01 18:06:10 +0100

    Update Package.swift (min deployment same as project)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "PremierKit",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
