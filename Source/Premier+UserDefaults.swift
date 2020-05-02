cb330adf5b4430729d6740ed8a715cb1ae5463f3
Ricardo Pereira <m@ricardopereira.eu>
2020-05-02 09:06:08 +0100

    Add OptionalUserDefault and accept a suite name for shared UserDefaults using AppGroups

--- Source/Premier+UserDefaults.swift
+++ Source/Premier+UserDefaults.swift
@@ -12,18 +12,74 @@ import Foundation
 public struct UserDefault<T> {
     let key: String
     let defaultValue: T
+    let suiteName: String?
 
-    public init(_ key: String, defaultValue: T) {
+    public init(_ key: String, defaultValue: T, suiteName: String? = nil) {
         self.key = key
         self.defaultValue = defaultValue
+        self.suiteName = suiteName
+    }
+
+    private func userDefaults() -> UserDefaults {
+        var userDefaults = UserDefaults.standard
+        if let suiteName = suiteName, let suiteUserDefaults = UserDefaults(suiteName: suiteName) {
+            userDefaults = suiteUserDefaults
+        }
+        return userDefaults
     }
 
     public var wrappedValue: T {
         get {
-            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+            return userDefaults().object(forKey: key) as? T ?? defaultValue
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: key)
+            userDefaults().set(newValue, forKey: key)
         }
     }
+}
+
+@propertyWrapper
+public struct OptionalUserDefault<T> {
+    let key: String
+    let defaultValue: T?
+    let suiteName: String?
+
+    public init(_ key: String, defaultValue: T? = nil, suiteName: String? = nil) {
+        self.key = key
+        self.defaultValue = defaultValue
+        self.suiteName = suiteName
+    }
+
+    private func userDefaults() -> UserDefaults {
+        var userDefaults = UserDefaults.standard
+        if let suiteName = suiteName, let suiteUserDefaults = UserDefaults(suiteName: suiteName) {
+            userDefaults = suiteUserDefaults
+        }
+        return userDefaults
+    }
+
+    public var wrappedValue: T? {
+        get {
+            return userDefaults().object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            userDefaults().set(newValue, forKey: key)
+        }
+    }
+}
+
+private struct TestUserDefault {
+
+    @UserDefault(#function, defaultValue: "")
+    static var a: String
+
+    @UserDefault(#function, defaultValue: "", suiteName: "PremierKitGroup")
+    static var b: String
+
+    @OptionalUserDefault(#function)
+    static var c: String?
+
+    @OptionalUserDefault(#function, suiteName: "PremierKitGroup")
+    static var d: String?
+
 }
